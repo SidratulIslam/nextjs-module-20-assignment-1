277f0ae9994cc26c01845646b9dd2cf79eb42bf6
Sidratul Islam <sidratul@topofstacksoftware.com>
2023-11-03 21:39:44 +0600

    refactor

--- src/components/BlogDetails.jsx
+++ src/components/BlogDetails.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactHtmlParser from "react-html-parser";
+import ReactHtmlParser from "html-react-parser";
 const BlogDetails = ({ data }) => {
   return (
     <>
